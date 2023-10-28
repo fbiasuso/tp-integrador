883a7f82945590583c46e9e69d9e188498c9e77f
fbiasuso <f.biasuso@gmail.com>
2023-10-27 22:52:22 -0300

    code cleanup

--- js/index.js
+++ js/index.js
@@ -1,19 +1,8 @@
-const category = (e) => {
+const category = (value) => {
 
     let category = document.getElementById("category")
-    category.options.selectedIndex =  e;
-
-    /*  let btnText = e.target.id
-console.log(btnText) */
-
-/* let categorySelected = category.options[category.selectedIndex].text; */
-/* console.log(categorySelected) */
-/* let categorySelected = category.options.selectedIndex =  */
-/* categorySelected = "Pedro"; */
-
+    category.options.selectedIndex =  value;
 }
-
-
 
 const totalPrice = () =>{
     let amount = Number(document.getElementById("amount").value);
@@ -33,8 +22,7 @@ const totalPrice = () =>{
                 discount = 0;
             break;
     }
-
-    /* amount === 0 && amount++ */
+    
     if(amount === 0 || Number.isNaN(amount)){
         alert("La cantidad ingresada no es correcta. Por favor ingresa una cantidad de tickets válida")
         return
@@ -51,13 +39,6 @@ const totalPrice = () =>{
 }
 
 const resetForm = () =>{
-    /* let category = document.getElementById("category")
-    category.options.selectedIndex =  0;
-    document.getElementById("price").textContent = "Total a Pagar: $ ";
-    document.getElementById("amount").value = 0;
-    document.getElementById("name").value = "";
-    document.getElementById("lastname").value = "";
-    document.getElementById("email").value = ""; */
     document.getElementById("formTicket").reset()
     document.getElementById("price").textContent = "Total a Pagar: $ ";
 }
